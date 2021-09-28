02a9632f140afb173cc07884b8f5530989266115
Nick Lalic <nick.lalic@gmail.com>
2021-09-28 14:30:36 -0600

    ✨ added markers and dist/markers.csv

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
-from pathlib                      import Path
-from rohdeschwarz.instruments.vna import Vna
+import csv
+from   pathlib                      import Path
+from   rohdeschwarz.instruments.vna import Vna
 
 
 # constants
@@ -14,6 +15,7 @@ vna_log_file = data_path / 'scpi.log'
 diagram_file = data_path / 'diagram1.png'
 formatted_trace_data_file = data_path / 'trc1-formatted.csv'
 complex_trace_data_file   = data_path / 'trc1-complex.csv'
+markers_file = data_path / 'markers.csv'
 
 data_path.mkdir(exist_ok=True)
 
@@ -44,7 +46,7 @@ vna.preset()
 # configure channel
 channel = vna.channel(1)
 channel.start_frequency_Hz = 1e9
-channel.stop_frequency_Hz  = 20e9
+channel.stop_frequency_Hz  = 8e9
 channel.points             = 201
 channel.if_bandwidth_Hz    = 1e3
 channel.power_dBm          = -10
@@ -57,6 +59,18 @@ trace.format    = 'MLOG'
 trace.autoscale()
 assert not vna.errors
 
+# configure markers
+trace.markers = [1, 2, 3]
+
+marker1 = trace.marker(1)
+marker1.x = 1e9
+
+marker2 = trace.marker(2)
+marker2.x = 2e9
+
+marker3 = trace.marker(3)
+marker3.x = 3e9
+
 # configure diagram
 diagram = vna.diagram(1)
 diagram.title = 'S21 Magnitude (dB)'
@@ -66,6 +80,17 @@ assert not vna.errors
 vna.continuous_sweep = False
 vna.sweep()
 assert not vna.errors
+
+# save markers to csv
+with markers_file.open('w') as f:
+    # header
+    f.write('# name, x_Hz, y_dB\n')
+
+    # data
+    csv_writer = csv.writer(f)
+    for m in trace.markers:
+        marker = trace.marker(m)
+        csv_writer.writerow([marker.name, marker.x, marker.y])
 
 # save trace data
 trace.save_data_locally(str(formatted_trace_data_file))
